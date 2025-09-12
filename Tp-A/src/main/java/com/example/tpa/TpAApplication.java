fd5b43c2d6de3653de2a97798ed03a5289697939
Zoevelazquez0430 <zvelazquezzorzi@udesa.edu.ar>
2025-09-12 09:55:39 -0300

    primer commit

---
+++ Tp-A/src/main/java/com/example/tpa/TpAApplication.java
@@ -0,0 +1,13 @@
+package com.example.tpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TpAApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TpAApplication.class, args);
+    }
+
+}
---
+++ Tp-A/src/test/java/com/example/tpa/TpAApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TpAApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
