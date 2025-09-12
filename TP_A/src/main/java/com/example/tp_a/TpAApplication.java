e3169bc49a36372c4c8981c9f76e48c4e1ef6e25
Zoevelazquez0430 <zvelazquezzorzi@udesa.edu.ar>
2025-09-12 10:15:15 -0300

    2

--- TP_A/src/main/java/com/example/tp_a/TpAApplication.java
+++ TP_A/src/main/java/com/example/tp_a/TpAApplication.java
@@ -1,4 +1,4 @@
-package com.example.tpa;
+package com.example.tp_a;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- TP_A/src/test/java/com/example/tp_a/TpAApplicationTests.java
+++ TP_A/src/test/java/com/example/tp_a/TpAApplicationTests.java
@@ -1,4 +1,4 @@
-package com.example.tpa;
+package com.example.tp_a;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
