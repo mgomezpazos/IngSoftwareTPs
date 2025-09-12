804e8ee6b80af5f1b82c12c50921877d7a13dd6d
Zoevelazquez0430 <zvelazquezzorzi@udesa.edu.ar>
2025-09-12 10:46:43 -0300

    ooootra vez

---
+++ TP_GomezPazos_Velazquez/GiftCardSystem/src/main/java/com/example/giftcardsystem/GiftCardSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.giftcardsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GiftCardSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GiftCardSystemApplication.class, args);
+    }
+
+}
---
+++ TP_GomezPazos_Velazquez/GiftCardSystem/src/test/java/com/example/giftcardsystem/GiftCardSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.giftcardsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GiftCardSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
